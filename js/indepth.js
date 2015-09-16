8806fc7b9f9d51e9bdb4e278b9cd62b304029c09
Rodrigo Luis Santander <roidersp@gmail.com>
2015-09-15 19:41:19 -0500

    ubhnjk

--- js/indepth.js
+++ js/indepth.js
@@ -4,17 +4,10 @@ var disqus_url="adivina-quien";
 var disqus_number_c=2;
 var disqus_per_page=3;
 var tamaño_total=1920;
-var marcas={"adidas":true,"nike":true,"new-balance":true,"underarmour":true,"puma":true,"otros":true};
+var marcas={"adidas":true,"nike":true,"newbalance":true,"underarmour":true,"puma":true,"otros":true};
 
 var ventana_alto = $(window).height();
 var ventana_ancho = $(window).width();
-
-
-
-
-$(document).on("click","path",function(){
-	alert("test");
-});
 
 $(document).on("click", "#indepth_share_twiiter", function(){
 	var text = encodeURIComponent("¡"+tecnico+" es mi técnico del Tri! Lo descubrí jugando Adivina Quién");
@@ -30,9 +23,6 @@ $(document).on("click", "#indepth_share_fb", function(){
 
 });
 
-
-
-
 var indepth_sizeAdjust = function(firstTime){
 	$(".indepth_page").each(function(){
 		if($(this).attr("resize") == "true"){
@@ -44,9 +34,6 @@ var indepth_sizeAdjust = function(firstTime){
 		}
 	})
 }
-
-
-
 
 var indepth_preloadImgs = function(){
 	$("img[over]").each(function(){
@@ -153,20 +140,19 @@ $(document).ready(function(){
 	 
 	 console.log(ventana_alto);
 	 
-	 $("#pais_cont").css({
+	/* $("#pais_cont").css({
 		width: ventana_ancho+"px",
 		height: ventana_alto+"px"
-	});
+	});*/
 
 
 	//$("#indepth_break_2").css("height",)
 	if(navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.platform == 'iPod' || navigator.platform == 'Android')
     {   	
-    	 $('#indepth_cover_view').css("position","absolute");
     }else{
-    	$('#indepth_cover').css("height",(ventana_alto-100)+"px");
-    	 if(ventana_alto>600){
-	 	$('.indepth_break').css("height",ventana_alto+"px");
+    	
+    	 if(ventana_ancho>600){
+	    	 $('#indepth_cover').css("height",(ventana_alto-100)+"px");
 
 
 	 	//$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
@@ -178,14 +164,12 @@ $(document).ready(function(){
     if(navigator.platform == 'iPad'){
 	    //$("#indepth_parallax_back").css("background-size", "100%");
 	    $("#indepth_parallax_back").css("background-attachment", "initial");
-	    console.log("ipad")
     }
 		loadDisqus($("#indepth_coments"),disqus_url, "http://juanfutbol.com/indepth/"+disqus_url);
 });
 
 $(window).on("resize", function(){
 	indepth_sizeAdjust(false);
-    	$('#indepth_cover').css("height",(ventana_alto-100)+"px");
     	 if(ventana_alto>600){
 	 	$('.indepth_break').css("height",ventana_alto+"px");
 	 	//$('#indepth_cover .indepth_cover_back_body').css("top",ventana_alto*.60);
@@ -203,13 +187,103 @@ $(window).on("resize", function(){
     }
 })
 
+var cont=$("#pais_cont");
 
+$.getJSON( urlIndepth+"js/data.json", function( data ) {
+	
+	
+	
+	$.each(data, function( i, item ) {
+		var option='<option value="'+i+'">'+item["nombre"]+'</option> ';
+		$("#seleccionador_movil select").append(option);
+		
+	});
+	
+	if(ventana_alto>600){
+		var pais=data['albania'];
+		if(pais!=undefined){
+			$("#pais_nombre").html(pais["nombre"]);
+			var champs=pais["champ"];
+			var euro=pais["euro"];
+			if(champs!=undefined){
+				console.log(champs);
+				$.each(champs, function( i, item ) {
+					var nombre=item["nombre"];
+					var images="images/equipos_logos/"+normalize(nombre).replace(/\s/g,"-").toLowerCase()+".png";
+					var div='<div class="pais_item_cont"><div class="pais_item" ><div class="pais_logo" style="background-image: url('+images+')"></div><div class="equipo_eq">'+nombre+'</div></div><div class="pais_item"><div class="pais_marca '+item["marca"].toLowerCase()+'"></div> </div></div>'
+					cont.find(".pais_datos").append(div);
+				});
+			}
+			
+			if(euro!=undefined){
+				console.log(euro);
+				$.each(euro, function( i, item ) {
+					var nombre=item["nombre"];
+					var images="images/equipos_logos/"+normalize(nombre).replace(/\s/g,"-").replace(".","").toLowerCase()+".png";
+					var div='<div class="pais_item_cont"><div class="pais_item" ><div class="pais_logo" style="background-image: url('+images+')"></div><div class="equipo_eq">'+nombre+'</div></div><div class="pais_item"><div class="pais_marca '+item["marca"].toLowerCase()+'"></div> </div></div>'
+					cont.find(".pais_datos").append(div);
+				});
+			}
+			cont.fadeIn();
+			cont.css("display","table");
+		}
+	}
+	
+	/*<div class="pais_item_cont">
+								<div class="pais_item" >
+									<div class="pais_logo" style="background-image: url(images/equipos_logos/ajax.png)">
+									
+									</div>
+									<div class="equipo_eq">
+										Real Madrid
+									</div>
+								</div>
+								<div class="pais_item"><div class="pais_marca adidas"></div> </div>
+							</div>
+	*/
+	$(document).on("click","#europa_paises .pais", function(){
+		var pais_id=$(this).attr("id");
+		var pais=data[pais_id];
+		cont.find(".pais_datos").html("");
+		if(pais!=undefined){
+			$("#pais_nombre").html(pais["nombre"]);
+			var champs=pais["champ"];
+			var euro=pais["euro"];
+			if(champs!=undefined){
+				console.log(champs);
+				$.each(champs, function( i, item ) {
+					var nombre=item["nombre"];
+					var images="images/equipos_logos/"+normalize(nombre).replace(/\s/g,"-").toLowerCase()+".png";
+					var div='<div class="pais_item_cont"><div class="pais_item" ><div class="pais_logo" style="background-image: url('+images+')"></div><div class="equipo_eq">'+nombre+'</div></div><div class="pais_item"><div class="pais_marca '+item["marca"].toLowerCase()+'"></div> </div></div>'
+					cont.find(".pais_datos").append(div);
+				});
+			}
+			
+			if(euro!=undefined){
+				console.log(euro);
+				$.each(euro, function( i, item ) {
+					var nombre=item["nombre"];
+					var images="images/equipos_logos/"+normalize(nombre).replace(/\s/g,"-").replace(".","").toLowerCase()+".png";
+					var div='<div class="pais_item_cont"><div class="pais_item" ><div class="pais_logo" style="background-image: url('+images+')"></div><div class="equipo_eq">'+nombre+'</div></div><div class="pais_item"><div class="pais_marca '+item["marca"].toLowerCase()+'"></div> </div></div>'
+					cont.find(".pais_datos").append(div);
+				});
+			}
+			cont.fadeIn();
+			cont.css("display","table");
+		}
+		
+		
+	});
+	
+});
 
+$(document).on("click","#pais_close",function(){
+	cont.fadeOut("slow");
+});
 
 
 $(document).on({
 	mouseenter: function () {
-		console.log( $(this).find(".st0"));
 		
         $(this).find(".st0").attr("class","st0 pais_hover");
     },
@@ -218,13 +292,21 @@ $(document).on({
     }
 },"#europa_paises .pais");
 
+var copa="champ";
 
 $(document).on("click", "#selec_izq", function(){
 	$(".selec_titulo").animate({
 		"margin-left":"0px"
 	},300);
 	$(this).hide();
+	$(".euro").hide();
+	$(".champ").show();
+	copa="champ";
 	$("#selec_der").show();
+	/*$(".marca_champ").show();
+	$(".marca_euro").hide();*/
+	$(".marcas_item").removeClass("apagado");
+	marcas={"adidas":true,"nike":true,"newbalance":true,"underarmour":true,"puma":true,"otros":true};
 });
 
 $(document).on("click", "#selec_der", function(){
@@ -232,7 +314,14 @@ $(document).on("click", "#selec_der", function(){
 		"margin-left":"-100%"
 	},300);
 	$(this).hide();
+	$(".champ").hide();
+	$(".euro").show();
 	$("#selec_izq").show();
+	copa="euro";
+	$(".marcas_item").removeClass("apagado");
+	marcas={"adidas":true,"nike":true,"newbalance":true,"underarmour":true,"puma":true,"otros":true};
+	/*$(".marca_champ").hide();
+	$(".marca_euro").show();*/
 });
 
 $(document).on("click",".marcas_item", function(){
@@ -247,11 +336,32 @@ $(document).on("click",".marcas_item", function(){
 	}else{
 		$(this).removeClass("apagado");
 		marcas[marca_item]=true;
-		$("."+marca_item).show();
+		$("."+marca_item+"."+copa).show();
+		marcas={"adidas":true,"nike":true,"newbalance":true,"underarmour":true,"puma":true,"otros":true};
 	}
 	
 });
 
-
+var normalize = (function() {
+  var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇçğńň", 
+      to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunnccgnn",
+      mapping = {};
+ 
+  for(var i = 0, j = from.length; i < j; i++ )
+      mapping[ from.charAt( i ) ] = to.charAt( i );
+ 
+  return function( str ) {
+      var ret = [];
+      for( var i = 0, j = str.length; i < j; i++ ) {
+          var c = str.charAt( i );
+          if( mapping.hasOwnProperty( str.charAt( i ) ) )
+              ret.push( mapping[ c ] );
+          else
+              ret.push( c );
+      }      
+      return ret.join( '' );
+  }
+ 
+})();
 
 
